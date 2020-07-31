408ff9aa0611e25439a83b770a99b0d5343de476
theognis1002 <theognis1002@gmail.com>
2020-07-30 21:10:18 -0400

    ci test

--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-
+def test():
+    print("hi")
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
